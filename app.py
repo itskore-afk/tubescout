c0ac72572f19508506cbfa86d0bf5530ee6d0025
itskore-afk <itskore.builds@proton.me>
2026-06-23 17:42:39 +0600

    Restore youtube extractor API

--- app.py
+++ app.py
@@ -1,10 +1,55 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import yt_dlp
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def home():
     return "TubeScout Backend Online"
 
+
+@app.route("/extract", methods=["POST"])
+def extract():
+
+    data = request.get_json()
+    url = data.get("url")
+
+    if not url:
+        return jsonify({"error": "Missing URL"}), 400
+
+    ydl_opts = {
+        "quiet": True,
+        "extract_flat": True,
+        "skip_download": True,
+        "user_agent": "Mozilla/5.0"
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+
+        urls = []
+
+        if "entries" in info:
+            for video in info["entries"]:
+                if video:
+                    urls.append(
+                        "https://youtube.com/watch?v=" + video["id"]
+                    )
+
+        return jsonify({
+            "count": len(urls),
+            "urls": urls
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
